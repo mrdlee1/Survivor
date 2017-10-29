9daf446761de4438f2246484cd42c72ced153036
mrdlee1 <mordleewan@gmail.com>
2017-10-29 23:15:42 +0500

    Improved the movement module and started to complete the function for 'walking'.

--- movement.py
+++ movement.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
-class movement:
+class mapControl:
 
     def __init__(self):
         #a 5X5 grid in which the map can be drawn
         self.mapgrid = []
+        self.currentPos = 12  # as a placeholder. 12 is the center
         for i in range(0, 25):
             self.mapgrid.append('@')
 
@@ -16,9 +17,22 @@ class movement:
             print " ".join(map(str, self.mapgrid[15:20]))
             print " ".join(map(str, self.mapgrid[20:25]))
 
-    def walk(self):
-        currentPos = 12  # as a placeholder. 12 is the center
-        self.mapgrid[currentPos] = '#'
-        self.drawmap()
+    def walk(self, direction):
+        error = False
+        if direction == 'north':
+            if self.currentPos > 5:
+                self.currentPos = self.currentPos - 5
+            else:
+                print "Placeholder text."
+                error = True
+        elif direction == 'south':
+            self.currentPos = self.currentPos + 5
+        elif direction == 'east':
+            self.currentPos = self.currentPos + 1
+        elif direction == 'west':
+            self.currentPos = self.currentPos - 1
+        self.mapgrid[self.currentPos] = '#'
+        if error is False:
+            self.drawmap()
 
-movement()
+mapControl()
